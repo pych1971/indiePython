1c485d5acdc1ed3960df651d9d6bde92d1a64e9c
pych1971 <pych1971@gmail.com>
2023-12-19 23:02:17 +0500

    191220232302

---
+++ 1/1_5/1_5_10.py
@@ -0,0 +1,10 @@
+h1=int(input())
+m1=int(input())
+s1=int(input())
+h2=int(input())
+m2=int(input())
+s2=int(input())
+sek1=h1*60**2+m1*60+s1
+sek2=h2*60**2+m2*60+s2
+interval=sek2-sek1
+print(interval)
