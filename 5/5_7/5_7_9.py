23a247e814feb0cb07acb79b16d89dc3e73534c0
pych1971 <pych1971@gmail.com>
2023-12-19 20:45:18 +0500

    191220232045

--- 5/5_7/5_7_9.py
+++ 5/5_7/5_7_9.py
@@ -1,1 +1,16 @@
-a=
+n, m = map(int, input().split())
+a = []
+s = 0
+x = 0
+ss = 0
+for i in range(n):
+    a.append(list(map(int, input().split())))
+for i in range(n):
+    ss = 0
+    for j in range(m):
+        ss += a[i][j]
+    if ss > s:
+        s = ss
+        x = i
+print(s)
+print(x)
