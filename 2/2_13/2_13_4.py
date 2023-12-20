a83a9a36b8b7f06a8bcbbb2bf936de0c9b51a080
pych1971 <pych1971@gmail.com>
2023-12-20 10:48:11 +0500

    201220231048

---
+++ 2/2_13/2_13_4.py
@@ -0,0 +1,7 @@
+numbers = [-214, 181, -139, 448, -20, -917, 32, 422, -895, 198, 284, 472, -986, -964, -989, 29]
+a=numbers.pop(-1)
+b=numbers.pop(0)
+c=numbers.pop(12)
+d=numbers.pop(7)
+print(numbers)
+print(a+b+c+d)
