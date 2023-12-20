8521702a10f1a1c9f93849afd5f9ca213e907948
pych1971 <pych1971@gmail.com>
2023-12-20 11:09:11 +0500

    201220231109

---
+++ 3/3_3/3_3_6.py
@@ -0,0 +1,8 @@
+s,v1,v2,t1,t2=map(int, input().split())
+if s*v1+2*t1==s*v2+2*t2:
+    print('Friendship')
+else:
+    if s*v1+2*t1>s*v2+2*t2:
+        print('Second')
+    else:
+        print('First')
