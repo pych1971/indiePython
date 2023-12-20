42a36d21b7edec1ac62b5052b1b96657bb4307cf
pych1971 <pych1971@gmail.com>
2023-12-20 11:11:58 +0500

    201220231111

---
+++ 3/3_4/3_4_5.py
@@ -0,0 +1,15 @@
+a=float(input())
+b=float(input())
+c=input()
+if b==0 and c=='/':
+    print('Неизвестно')
+elif c=='+':
+    print(a+b)
+elif c=='-':
+    print(a-b)
+elif c=='*':
+    print(a*b)
+elif c=='/':
+    print(a/b)
+else:
+    print('Неизвестно')
