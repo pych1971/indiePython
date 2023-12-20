a04f8bcc44d33d7e768be041653e4ac6ce522a14
pych1971 <pych1971@gmail.com>
2023-12-20 07:47:13 +0500

    201220230747

---
+++ 2/2_1/2_1_10.py
@@ -0,0 +1,4 @@
+a,b,c=map(str, input().split())
+print('Simvol code ', a, ' is ', ord(a), '.', sep='')
+print('Simvol code ', b, ' is ', ord(b), '.', sep='')
+print('Simvol code ', c, ' is ', ord(c), '.', sep='')
