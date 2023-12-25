da9a11b2d106875f386468d149ccc8613b2134a4
pych1971 <pych1971@gmail.com>
2023-12-25 18:48:27 +0500

    251220231848

---
+++ 6/6_11/6_11_5.py
@@ -0,0 +1,14 @@
+words = ['mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
+         'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error',
+         'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
+         'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
+         'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention', 'tradition',
+         'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal', 'government',
+         'control', 'value', 'few', 'generation', 'service', 'national',
+         'tradition', 'government', 'mention', 'proposal']
+words_set = set(words)
+s = 0
+for i in words_set:
+    if len(i) > 6:
+        s += 1
+print(s)
