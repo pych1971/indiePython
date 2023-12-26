c9d966d108d3ed8f193ccc076cc07f1cd6bd465e
pych1971 <pych1971@gmail.com>
2023-12-26 20:38:13 +0500

    261220232038

---
+++ 6/6_12/6_12_1.py
@@ -0,0 +1,3 @@
+my_set = {'government', 'control', 'winter', 'few', 'generation',
+          'service', 'national', 'tradition', 'government'}
+my_set.update(['concert', 'brown', 'jacket', 'value'])
