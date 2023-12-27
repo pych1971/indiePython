b2f137bb3514e628403fc3ceefe5725ff9ce9f41
pych1971 <pych1971@gmail.com>
2023-12-27 20:56:58 +0500

    271220232056

---
+++ 6/6_14/6_14_2.py
@@ -0,0 +1,6 @@
+english_words = ('attack', 'bless', 'look', 'reckless', 'short', 'monster', 'trolley', 'sound',
+                 'ambiguity', 'researcher', 'trunk', 'coat', 'quantity', 'question', 'tenant',
+                 'miner', 'definite', 'kit', 'spectrum', 'satisfied', 'selection', 'carve',
+                 'ask', 'go', 'suggest')
+for index, value in enumerate(english_words, 1):
+    print(f'Word № {index} = {value}')
