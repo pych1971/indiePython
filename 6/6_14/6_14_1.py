fe01a8e83c2f4871d6d0b7513ca0dcb1567d43ef
pych1971 <pych1971@gmail.com>
2023-12-27 20:54:17 +0500

    271220232054

---
+++ 6/6_14/6_14_1.py
@@ -0,0 +1,7 @@
+words = ['feel', 'graduate', 'movie', 'fashionable', 'bacon',
+         'drop', 'produce', 'acquisition', 'cheap', 'strength',
+         'master', 'perception', 'noise', 'strange', 'am']
+words_with_position = []
+for index, value in enumerate(words, 1):
+    words_with_position.append((value, index))
+print(words_with_position)
