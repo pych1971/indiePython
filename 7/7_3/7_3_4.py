130de9b59da024df4d3186794c6b3f221fc14a4e
pych1971 <pych1971@gmail.com>
2023-12-28 20:34:45 +0500

    281220232034

---
+++ 7/7_3/7_3_4.py
@@ -0,0 +1,12 @@
+def generate_fizz_buzz_list(n):
+    a = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            a.append('FizzBuzz')
+        elif i % 3 == 0:
+            a.append('Fizz')
+        elif i % 5 == 0:
+            a.append('Buzz')
+        else:
+            a.append(i)
+    return (a)
