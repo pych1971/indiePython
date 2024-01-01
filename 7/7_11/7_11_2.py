ef084fcdd678420cbf3564a2ffafbf8ba4e87d1b
pych1971 <pych1971@gmail.com>
2024-01-01 21:09:35 +0500

    010120242109

---
+++ 7/7_11/7_11_2.py
@@ -0,0 +1,6 @@
+subject_marks = [('English', 88), ('Science', 90), ('Maths', 97),
+                 ('Physics', 93), ('History', 82), ('French', 78),
+                 ('Art', 58), ('Chemistry', 76), ('Programming', 91)]
+a = sorted(subject_marks, key=lambda x: x[1], reverse=True)
+for i in a:
+    print(i[0], i[1])
