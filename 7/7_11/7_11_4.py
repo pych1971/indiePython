5e046bc7727f72056757a8c53f8384ad3d581712
pych1971 <pych1971@gmail.com>
2024-01-02 20:14:43 +0500

    020120242014

---
+++ 7/7_11/7_11_4.py
@@ -0,0 +1,10 @@
+models = [{'make': 'Nokia', 'model': 216, 'color': 'Black'},
+          {'make': 'Mi Max', 'model': 2, 'color': 'Gold'},
+          {'make': 'Samsung', 'model': 7, 'color': 'Blue'},
+          {'make': 'Apple', 'model': 10, 'color': 'Silver'},
+          {'make': 'Oppo', 'model': 9, 'color': 'Red'},
+          {'make': 'Huawei', 'model': 4, 'color': 'Grey'},
+          {'make': 'Honor', 'model': 3, 'color': 'Black'}]
+a = sorted(models, key=lambda x: x['color'])
+for i in a:
+    print(f"Производитель: {i['make']}, модель: {i['model']}, цвет: {i['color']}")
