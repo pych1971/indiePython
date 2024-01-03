6f2e4423cb861e623dc88db875629941902c82e8
pych1971 <pych1971@gmail.com>
2024-01-03 16:48:25 +0500

    030120241648

---
+++ 7/7_12/7_12_2.py
@@ -0,0 +1,26 @@
+def calculate(x: float, y: float, operation: str = 'a') -> None:
+    def addition():
+        print(float(x + y))
+
+    def subtraction():
+        print(float(x - y))
+
+    def division():
+        if y == 0:
+            print('На ноль делить нельзя!')
+        else:
+            print(float(x / y))
+
+    def multiplication():
+        print(float(x * y))
+
+    if operation == 'a':
+        addition()
+    elif operation == 's':
+        subtraction()
+    elif operation == 'd':
+        division()
+    elif operation == 'm':
+        multiplication()
+    else:
+        print('Ошибка. Данной операции не существует')
