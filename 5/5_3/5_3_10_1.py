42658706cc72777839302dc755881eff8790fb4a
pych1971 <pych1971@gmail.com>
2024-02-08 12:37:47 +0500

    080220241237

---
+++ 5/5_3/5_3_10_1.py
@@ -0,0 +1,9 @@
+spisok = list(map(int, input().split()))
+result = 'True'
+for i in range(len(spisok) - 1):
+    if spisok[i] <= spisok[i + 1]:
+        continue;
+    else:
+        result = 'False'
+        break;
+print(result)
