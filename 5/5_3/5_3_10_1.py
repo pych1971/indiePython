3c680bdce48ad2fd7a3493221aba79b5a63bfd30
pych1971 <pych1971@gmail.com>
2024-02-08 14:02:29 +0500

    080220241402

--- 5/5_3/5_3_10_1.py
+++ 5/5_3/5_3_10_1.py
@@ -2,8 +2,8 @@ spisok = list(map(int, input().split()))
 result = 'True'
 for i in range(len(spisok) - 1):
     if spisok[i] <= spisok[i + 1]:
-        continue;
+        continue
     else:
         result = 'False'
-        break;
+        break
 print(result)
